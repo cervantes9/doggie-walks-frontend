f9f0e377b4af489a6d01c3f568340d052a26eaa5
De Jesus Sierra <djcmsh0322@gmail.com>
2026-04-17 22:12:08 +0200

    primer commit - frontend doggie walks

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+/*import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -7,5 +7,24 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes)
+  ]
+};*/
+// Este el panel de control o configuración global osea app.config.ts
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+//importa de angular  tipos de datos y captura de errores
+import { provideRouter } from '@angular/router'; // navegar entre paginas  Por ejemplo, ir de /inicio a /login.
+import { routes } from './app.routes';
+//es la lista de rutas que tu defines en el archivo app.routes.ts
+
+// NUEVO 1: esta herramienta nos sirve para llamar  al backend con  GET, POST ETC
+import { provideHttpClient } from '@angular/common/http';
+
+export const appConfig: ApplicationConfig = { //esto es javascript lo que esta haciendo
+  //llamando a las propiedades de ApplicationConfig el nombre de la variable es appConfig
+  // es constante y publica con export.
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes),
+    provideHttpClient() // NUEVO 2: Encendemos la herramienta (¡Ojo con poner la coma en la línea de arriba!)
   ]
 };
