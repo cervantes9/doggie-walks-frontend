65ed0bc6a2266ed03bf658dab640fce6cd2d57eb
De Jesus Sierra <djcmsh0322@gmail.com>
2026-05-02 11:40:27 +0200

    sección paseadores conectada al backend con CSS

--- src/app/componentes/paseadores/paseadores.ts
+++ src/app/componentes/paseadores/paseadores.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-paseadores',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './paseadores.html',
   styleUrl: './paseadores.css',
 })
-export class Paseadores {}
+export class Paseadores {
+
+  // Como el @Autowired de Spring Boot, inyectamos el HttpClient
+  private http = inject(HttpClient);
+
+  // El canal de datos que apunta al endpoint de paseadores
+  listaPaseadores$ = this.http.get<any[]>('http://localhost:8080/api/paseadores');
+
+}
