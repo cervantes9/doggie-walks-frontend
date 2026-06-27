40f900899a53fd67c1b8ff149cf62222d792651b
De Jesus Sierra <djcmsh0322@gmail.com>
2026-06-27 23:28:42 +0200

    enlaces reales en redes sociales, FAQ depurada y galeria ampliada con boton volver arriba

--- src/app/componentes/galeria/galeria.ts
+++ src/app/componentes/galeria/galeria.ts
@@ -16,6 +16,12 @@ export class Galeria {
   { id: 3, url: 'https://images.pexels.com/photos/14116508/pexels-photo-14116508.jpeg?auto=compress&cs=tinysrgb&w=800', alt: 'Perro feliz en paseo' },
   { id: 4, url: 'https://images.pexels.com/photos/22236325/pexels-photo-22236325.jpeg?auto=compress&cs=tinysrgb&w=800', alt: 'Entrenamiento canino' },
   { id: 5, url: 'https://images.pexels.com/photos/7210693/pexels-photo-7210693.jpeg?auto=compress&cs=tinysrgb&w=800', alt: 'Paseo en el parque' },
+  { id: 6, url: 'https://images.pexels.com/photos/30881216/pexels-photo-30881216.jpeg?auto=compress&cs=tinysrgb&w=800', alt: 'Un dálmata y un golden retriever se saludan en una acera de la ciudad' },
+  { id: 7, url: 'https://images.pexels.com/photos/17953806/pexels-photo-17953806.jpeg?auto=compress&cs=tinysrgb&w=800', alt: 'Perro paseando por el pavimento de una calle urbana' },
+  { id: 8, url: 'https://images.pexels.com/photos/20140406/pexels-photo-20140406.jpeg?auto=compress&cs=tinysrgb&w=800', alt: 'Perro disfrutando de un paseo al aire libre en verano' },
+  { id: 9, url: 'https://images.pexels.com/photos/36417031/pexels-photo-36417031.jpeg?auto=compress&cs=tinysrgb&w=800', alt: 'Paseador con varios perros por las calles de la ciudad en verano' },
+  { id: 10, url: 'https://images.pexels.com/photos/19773192/pexels-photo-19773192.jpeg?auto=compress&cs=tinysrgb&w=800', alt: 'Perro jugando sobre el césped en un parque' },
+  { id: 11, url: 'https://images.pexels.com/photos/29806392/pexels-photo-29806392.jpeg?auto=compress&cs=tinysrgb&w=800', alt: 'Perro corriendo feliz con una pelota en un parque soleado' },
 ];
 
   // -1 significa que no hay foto seleccionada y el modal está cerrado
@@ -29,6 +35,10 @@ export class Galeria {
   // Cierra el modal
   cerrarFoto() {
     this.fotoSeleccionada = -1;
+  }
+
+  volverArriba() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
   }
 
   // Va a la foto anterior
