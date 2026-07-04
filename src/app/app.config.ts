a03c2c5828db299ac9073e3319ad85356d978d4d
De Jesus Sierra <djcmsh0322@gmail.com>
2026-07-04 13:59:30 +0200

    unificar verdes corporativos: foco de reservas, experiencia y badge de paseadores

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,4 @@
-/*import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
-
-export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
-};*/
 // Este el panel de control o configuración global osea app.config.ts
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 //importa de angular  tipos de datos y captura de errores
